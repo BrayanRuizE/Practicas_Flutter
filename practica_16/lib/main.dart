54f96d139d9e82358f69cfcd5893b2aceeb14ba7
BrayanRuizE <brayanalrues11@gmail.com>
2025-10-14 20:27:20 -0700

    Practica 16

---
+++ practica_16/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:practica_16/src/listview_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: ListviewPage(),
+    );
+  }
+}
---
+++ practica_16/lib/src/listview_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:practica_16/src/animated_page.dart';
+import 'package:practica_16/src/tween_page.dart';
+
+class ListviewPage extends StatelessWidget {
+  const ListviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Practica 16 - Animaciones'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Animated'),
+            trailing: Icon(Icons.arrow_right),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AnimatedPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Tween'),
+            trailing: Icon(Icons.arrow_right),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TweenPage()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
